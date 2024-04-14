6a63a374cc2580c39f7ac594b88b53d4fdbf6b83
mmalik2004 <mehakmalik1282@gmail.com>
2024-04-14 19:59:47 +0530

    first commit

---
+++ lib/model/todo.dart
@@ -0,0 +1,27 @@
+class ToDo {
+  String? id;
+  String? todoText;
+  bool isDone;
+
+  ToDo({
+    required this.id,
+    required this.todoText,
+    this.isDone = false,
+  });
+
+  static List<ToDo> todoList() {
+    return [
+      ToDo(id: '01', todoText: 'Morning Excercise', isDone: true ),
+      ToDo(id: '02', todoText: 'Buy Groceries', isDone: true ),
+      ToDo(id: '03', todoText: 'Check Emails', ),
+      ToDo(id: '04', todoText: 'Team Meeting', ),
+      ToDo(id: '05', todoText: 'Work on mobile apps ', ),
+      ToDo(id: '06', todoText: 'Dinner with Jenny', ),
+      ToDo(id: '07', todoText: 'Breakfast', ),
+      ToDo(id: '08', todoText: 'Notes making', ),
+      
+    ];
+    
+    
+  }
+}
